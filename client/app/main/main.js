dd714bfd40a79b5dcc8bf7e8cd7c34f00a563d71
David Akhsakhalyan <dakhsakhalyan@macadamian.com>
2016-04-10 02:21:11 +0400

    Added main page

--- client/app/main/main.js
+++ client/app/main/main.js
@@ -1,3 +1,11 @@
-/**
- * Created by davidakhsakhalyan on 4/10/16.
- */
+'use strict';
+angular.module('sweetboxApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl',
+        authenticate: true
+      });
+  });
