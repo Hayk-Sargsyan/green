f727998d25fc20bb71ad21742aedf5f1e38f897c
Armen <armenzakaryan@Zakaryans-MacBook-Pro.local>
2016-04-09 23:47:10 +0400

    added sweets GRUD

--- server/api/sweets/index.js
+++ server/api/sweets/index.js
@@ -6,7 +6,13 @@ var controller = require('./sweets.controller');
 
 var router = express.Router();
 
+
 router.get('/', controller.all);
+router.get('/:number', controller.show);
 router.post('/', controller.create);
+router.put('/:number', controller.update);
+router.patch('/:number', controller.update);
+router.delete('/:number', controller.destroy);
+
 
 module.exports = router;
--- server/api/sweets/sweets.controller.js
+++ server/api/sweets/sweets.controller.js
@@ -64,25 +64,57 @@ function handleError(res, statusCode) {
  * Get list of users
  * restriction: 'admin'
  */
-exports.all = function(req, res) {
-  return res.status(200).json([
-  	{"name": "sweet_1", "price": "100", "count": 9, "number": 123 },
-  	{"name": "sweet_2", "price": "500", "count": 3, "number": 124 },
-  	{"name": "sweet_3", "price": "50", "count": 32, "number": 125 },
-  	{"name": "sweet_4", "price": "40", "count": 29, "number": 126 },
-  	{"name": "sweet_5", "price": "700", "count": 79, "number": 127 },
-  	{"name": "sweet_6", "price": "900", "count": 95, "number": 128 }
-  ]);
-};
+// exports.all = function(req, res) {
+//   return res.status(200).json([
+//   	{"name": "sweet_1", "price": "100", "count": 9, "number": 123 },
+//   	{"name": "sweet_2", "price": "500", "count": 3, "number": 124 },
+//   	{"name": "sweet_3", "price": "50", "count": 32, "number": 125 },
+//   	{"name": "sweet_4", "price": "40", "count": 29, "number": 126 },
+//   	{"name": "sweet_5", "price": "700", "count": 79, "number": 127 },
+//   	{"name": "sweet_6", "price": "900", "count": 95, "number": 128 }
+//   ]);
+// };
 
 
+// Gets a list of Things
+export function all(req, res) {
+  return Sweet.find().exec()
+    .then(respondWithResult(res))
+    .catch(handleError(res));
+}
 
+// Gets a single Thing from the DB
+export function show(req, res) {
+  return Sweet.findByNumber(req.params.number).exec()
+    .then(handleEntityNotFound(res))
+    .then(respondWithResult(res))
+    .catch(handleError(res));
+}
 
 // Creates a new Thing in the DB
 export function create(req, res) {
-  console.log("saving sweets", req);
-
+  console.log("saving sweets", req.body);
   return Sweet.create(req.body)
     .then(respondWithResult(res, 201))
+    .catch(handleError(res));
+}
+
+// Updates an existing Thing in the DB
+export function update(req, res) {
+  if (req.body._id) {
+    delete req.body._id;
+  }
+  return Sweet.findByNumber(req.params.number).exec()
+    .then(handleEntityNotFound(res))
+    .then(saveUpdates(req.body))
+    .then(respondWithResult(res))
+    .catch(handleError(res));
+}
+
+// Deletes a Thing from the DB
+export function destroy(req, res) {
+  return Sweet.findByNumber(req.params.number).exec()
+    .then(handleEntityNotFound(res))
+    .then(removeEntity(res))
     .catch(handleError(res));
 }
